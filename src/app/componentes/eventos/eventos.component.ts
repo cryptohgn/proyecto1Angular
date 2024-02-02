87c74a9aa8c10d32a316f83ff779255a23ba864d
hanseldorf19 <hanseldorf19@gmail.com>
2024-02-02 12:22:14 +0100

    Añadida funcionalidad obteniendo y renderizando valor del input

--- src/app/componentes/eventos/eventos.component.ts
+++ src/app/componentes/eventos/eventos.component.ts
@@ -10,4 +10,15 @@ export class EventosComponent {
   seleccionarOpcion($event: any) {
    this.opcionSeleccionada = $event.target.value;
   }
+  valorDelInput: string ="";
+  obtenerValorInput($event: any){
+    this.valorDelInput = $event.target.value;
+  }
 }
+
+// Dentro del componente Eventos vamos a colocar un campo de texto
+// ```html
+// <input type="text">
+// ```
+//   - Recuperamos el valor escrito en el campo de texto y lo mostramos en un párrafo 
+//   - ¿Qué evento es el que me permite visualizar el cambio sobre el campo de texto?
